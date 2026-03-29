aaa949d2b08b4b490544312d4110b00c11783e9c
Wayne Bishop <waynewbishop@users.noreply.github.com>
2026-03-29 13:40:43 -0700

    fix: adding quiver 1.0.0 dependency..

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v15)
     ],
     dependencies: [
-        .package(url: "https://github.com/waynewbishop/quiver.git", branch: "main")
+        .package(url: "https://github.com/waynewbishop/quiver.git", from: "1.0.0")
     ],
     targets: [
         .target(
